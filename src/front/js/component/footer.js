1c97a9ec5f16d1cc780b2a2e81a1df7ad290cf68
Alain-Estrada <aestradap17@gmail.com>
2024-05-31 23:24:10 -0400

    sumando la plantilla y los cambios de themas

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+	<footer className="container">
+		<p className="float-end"><a href="#">Back to top</a></p>
+		<p>&copy; 2017–2024 Company, Inc. &middot; <a href="#">Privacy</a>
+			&middot;
+			<a href="#">Terms</a>
 		</p>
 	</footer>
 );
+
+
+
+// <footer className="footer mt-auto py-3 text-center">
+// 	<p>
+// 		Made with <i className="fa fa-heart text-danger" /> by{" "}
+// 		<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+// 	</p>
+// </footer>
