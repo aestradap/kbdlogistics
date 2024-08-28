1fed303c07c6acad5031623611ac74c57db488f4
Alain-Estrada <aestradap17@gmail.com>
2024-08-27 23:42:00 -0400

    arreglando focus azul sobre botones de carrousel y mal comportamiento mostrando boton de reproducisrse Pending: 1. DETALLES EL EL FRONT Y VISTA MOBIL 2. AGREGAR CAMBIO DE IDIOMA 3. TERMINAR PAGINAS EN DETALLE PARA CADA TIPO DE TRANSPORTACION.

--- src/front/js/component/BackgroundVideo.js
+++ src/front/js/component/BackgroundVideo.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 
-export const BackgroundVideo = ({ video }) => (
+export const BackgroundVideo = ({ video }) => {
+  const handlePlay = (e) => {
+    e.preventDefault();
+  };
 
-  <div className="video-container">
-    <video className="background-video" autoPlay loop muted>
+  return <div className="video-container">
+    <video className="background-video"
+           autoPlay
+           loop
+           muted
+           onClick={handlePlay}
+    >
       <source src={video} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     <div className="video-overlay">
-      {/* Aquí puedes poner cualquier contenido adicional
+      {/* cualquier contenido adicional
         que quieras mostrar sobre el video */}
     </div>
-  </div>);
+  </div>;
+};
+
+
 
 
 
