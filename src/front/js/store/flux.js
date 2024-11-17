d0278865a64986bdd07943b3a71797c79f3e3a4b
Alain-Estrada <aestradap17@gmail.com>
2024-11-17 18:43:06 -0500

    ultimando detalles2

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -154,7 +154,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           getActions().updateFinalQuote("manyDifDimeCargo", myQuote.manyDifDimeCargo);
         }
 
-        if (myQuote.comments != "") {
+        if (myQuote.comments) {
           getActions().updateFinalQuote("4", "4");
           getActions().updateFinalQuote("Comments", myQuote.comments);
         }
@@ -348,37 +348,9 @@ const getState = ({ getStore, getActions, setStore }) => {
         localStorage.setItem("step", value);
       },
 
-      getMessage: async () => {
-        try {
-          // fetching data from the backend
-          const resp = await fetch(process.env.BACKEND_URL + "api/hello");
-          const data = await resp.json();
-          setStore({ message: data.message });
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
-
       setLng: (lngValue) => {
         setStore({ lng: lngValue });
         localStorage.setItem("lng", lngValue);
-      },
-
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
-
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
-
-        //reset the global store
-        setStore({ demo: demo });
       }
     }
   };
